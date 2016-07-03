ed588dc2a7b2eb4a1325a08f107938e0c728eff8
ZheldakArtem <artemzheldak1995@gmail.com>
2016-07-03 17:15:03 +0300

    begin

--- EPAM.Summer.Day2.Zheldak/ForTask/Program.cs
+++ EPAM.Summer.Day2.Zheldak/ForTask/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task1;
 
 namespace ForTask
 {
@@ -10,6 +11,14 @@ namespace ForTask
     {
         static void Main(string[] args)
         {
+            double number = 255;
+            int exponent = 4;
+            double correctness = 0.01;
+            var result = NewtonMetjod.FindingTheRoot(number, exponent, correctness);
+            Console.WriteLine("NewtonMethod:{0}", result);
+            double resultPow = Math.Pow(number, (double)1/exponent);
+            Console.WriteLine("Math.Pow:{0}", resultPow);
+            Console.ReadLine();
         }
     }
 }
---
+++ EPAM.Summer.Day2.Zheldak/NewtonMethods/NewtonMetjod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public static class NewtonMetjod
+    {
+        public static double FindingTheRoot(double number, int exponent, double correctness)
+        {
+            if (number < 0 && exponent % 2 == 0)
+            {
+                return double.NaN;
+            }
+            if (exponent == 0)
+            {
+                return 1;
+            }
+            double x = number;
+            double xk = 0;
+            while (correctness < Math.Abs(x - xk))
+            {
+                xk = x;
+                x = 1.0 / exponent * ((exponent - 1) * xk + number / Math.Pow(xk, exponent - 1));
+            }
+            return x;
+        }
+    }
+}
