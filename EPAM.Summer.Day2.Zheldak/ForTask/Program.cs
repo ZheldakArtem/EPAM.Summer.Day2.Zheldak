111f4d8ecda25038bd5f08329b4bbdbc2c9bc207
ZheldakArtem <artemzheldak1995@gmail.com>
2016-07-04 20:20:48 +0300

    Adding xml comments and changing names of parameters.

--- EPAM.Summer.Day2.Zheldak/ForTask/Program.cs
+++ EPAM.Summer.Day2.Zheldak/ForTask/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Task1;
+using Task2;
 
 namespace ForTask
 {
@@ -11,14 +12,16 @@ namespace ForTask
     {
         static void Main(string[] args)
         {
-
+            
             double number = 255;
             int exponent = 4;
-            double correctness = 0.01;
-            var result = NewtonMetjod.FindingTheRoot(number, exponent, correctness);
+            double correctness = 0.001;
+            var result = NewtonMethod.FindingTheRoot(number, exponent, correctness);
             Console.WriteLine("NewtonMethod:{0}", result);
             double resultPow = Math.Pow(number, (double)1/exponent);
             Console.WriteLine("Math.Pow:{0}", resultPow);
+
+            EuclidAlgorithm.GetGCD(0, 0);
             Console.ReadLine();
         }
     }
--- EPAM.Summer.Day2.Zheldak/Task2/Euclid.cs
+++ EPAM.Summer.Day2.Zheldak/Task2/Euclid.cs
@@ -6,22 +6,33 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
-    public static class Euclid
+    public static class EuclidAlgorithm
     {
-        public static int GetGCD(int a, int b)
+        /// <summary>
+        /// The method for getting the greatest common divisor of two numbers.
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns>The geatest common divisor of two numbers</returns>
+        public static int GetGCD(int first, int second)
         {
-            while (a != 0 && b != 0)
+            if (first == 0 && second == 0)
             {
-                if (a > b)
-                    a %= b;
+                throw new ArgumentException();
+            }
+            while (first != 0 && second != 0)
+            {
+                if (first > second)
+                    first %= second;
                 else
-                    b %= a;
+                    second %= first;
             }
 
-            if (a == 0)
-                return b;
+            if (first == 0)
+                return second;
             else
-                return a;
+                return first;
         }
+        
     }
 }
