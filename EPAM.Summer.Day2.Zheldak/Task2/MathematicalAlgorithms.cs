d618b45ac17a237cfec34c49995edb1801b9bb92
ZheldakArtem <artemzheldak1995@gmail.com>
2016-07-05 20:11:14 +0300

    Add gcd methods

--- EPAM.Summer.Day2.Zheldak/Task2/MathematicalAlgorithms.cs
+++ EPAM.Summer.Day2.Zheldak/Task2/MathematicalAlgorithms.cs
@@ -74,7 +74,7 @@ namespace Task2
         /// <param name="second"></param>
         /// <param name="third"></param>
         /// <param name="elapsedTime"></param>
-        /// <returns></returns>
+        /// <returns>Great common devisor</returns>
         public static int GetGCD(int first, int second, int third, out long elapsedTime)
         {
             var stopWatch = new Stopwatch();
@@ -114,7 +114,7 @@ namespace Task2
         /// </summary>
         /// <param name="elapsedTime"> elapsed time</param>
         /// <param name="arrayNumbers">count of params</param>
-        /// <returns>GCD</returns>
+        /// <returns>Grear common devisir</returns>
         public static int GetGCD(out long elapsedTime, params int[] arrayNumbers)
         {
             var stopWatch = new Stopwatch();
@@ -172,6 +172,25 @@ namespace Task2
             return BinaryGCDalgorithm((second - first) / 2, first);
         }
         /// <summary>
+        /// The method for getting the greatest common divisor of two numbers and receiving method execution time.
+        /// </summary>
+        /// <param name="first"> First value</param>
+        /// <param name="second">Second value</param>
+        /// <param name="elapsedTime">Out param for elapsed time in millisecs</param>
+        /// <returns>The greatest common divisor of two numbers</returns>
+        public static int BinaryGCDalgorithm(int first, int second, out long elapsedTime)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            int result = BinaryGCDalgorithm(first, second);
+
+            stopWatch.Stop();
+            elapsedTime = stopWatch.ElapsedTicks;
+
+            return result;
+        }
+        /// <summary>
         /// The method for getting the greatest common divisor of three numbers.
         /// </summary>
         /// <param name="first">first value</param>
@@ -182,6 +201,26 @@ namespace Task2
         {
             int intermediate = BinaryGCDalgorithm(first, second);
             int result = BinaryGCDalgorithm(intermediate,third);
+
+            return result;
+        }
+        /// <summary>
+        /// The method for getting the greatest common divisor of three numbers and receiving method execution time.
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <param name="third"></param>
+        /// <param name="elapsedTime"></param>
+        /// <returns>great common devisor</returns>
+        public static int BinaryGCDalgorithm(int first, int second, int third,out long elapsedTime)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            int result = BinaryGCDalgorithm(first, second, third);
+
+            stopWatch.Stop();
+            elapsedTime = stopWatch.ElapsedTicks;
 
             return result;
         }
@@ -207,6 +246,36 @@ namespace Task2
 
             return resultArray.Min();
         }
+        /// <summary>
+        /// The method for getting the greatest common divisor(GCD) and receiving method execution time.
+        /// <exception cref="ArgumentException">Throws when you have less two params</exception>
+        /// </summary>
+        /// <param name="elapsedTime"> elapsed time</param>
+        /// <param name="arrayNumbers">count of params</param>
+        /// <returns>Grear common devisir</returns>
+        public static int BinaryGCDalgorithm(out long elapsedTime, params int[] arrayNumbers)
+        {
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            if (arrayNumbers.Length < 2)
+            {
+                throw new ArgumentException();
+            }
+
+            Array.Sort(arrayNumbers);
+            int[] resultArray = new int[arrayNumbers.Length - 1];
+            for (int i = 0; i < arrayNumbers.Length - 1; i++)
+            {
+                resultArray[i] = BinaryGCDalgorithm(arrayNumbers[0], arrayNumbers[i + 1]);
+            }
+
+            stopWatch.Stop();
+            elapsedTime = stopWatch.ElapsedTicks;
+
+            return resultArray.Min();
+        }
+
     }
 }
 
