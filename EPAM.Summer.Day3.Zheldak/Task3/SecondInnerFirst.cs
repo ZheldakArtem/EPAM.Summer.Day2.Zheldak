0fc28c0e032a9cd06cbb3cc5cc06c5d58a2e8072
ZheldakArtem <artemzheldak1995@gmail.com>
2016-07-06 10:36:56 +0300

    Add Method InsertMethod

---
+++ EPAM.Summer.Day3.Zheldak/Task3/SecondInnerFirst.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class SecondInnerFirst
+    {
+        static int InsertMethod(int first, int second, int startPos, int endPos)
+        {
+            return 1;
+        }
+    }
+}
