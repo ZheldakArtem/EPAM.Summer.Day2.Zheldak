d7c0ac9db7b3d7c953a786ffda1677d8b38c14b1
ZheldakArtem <artemzheldak1995@gmail.com>
2016-07-14 22:59:20 +0300

    Refactoring code

--- EPAM.Summer.Day2.Zheldak/Task2.UnitTest/MathematicalAlgorithmsTest.cs
+++ EPAM.Summer.Day2.Zheldak/Task2.UnitTest/MathematicalAlgorithmsTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -10,27 +12,110 @@ namespace Task2.NUnitTest
 {
     public class EucludianMathematicalAlgorithmsTests
     {
-        [TestCase(10, 15, ExpectedResult = 5)]
+        EuclidianDelegate methodGDC = GDCMethodForDelegate;
+        EuclidianDelegate methodBinary = BinaryMethodForDelegate;
+
+        [TestCase(10, 15,  ExpectedResult = 5)]
         [TestCase(20, 10, ExpectedResult = 10)]
         [TestCase(20, 0, ExpectedResult = 20)]
         [TestCase(-20, 15, ExpectedResult = 5)]
-        public int GetGCD_Test_With_Two_Arguments(int first, int second)
+        public int GetGCD_Test_With_Two_Arguments(int first, int second, EuclidianDelegate methodGDC)
         {
-            return EucludianMathematicalAlgorithms.GetGCD(first, second);
+            return EucludianMathematicalAlgorithms.GetGCD(first, second,methodGDC);
         }
         [TestCase(10, 15, 4, 1, ExpectedResult = 1)]
         [TestCase(20, 35, 10, 55, ExpectedResult = 5)]
-        [TestCase(20, 35, -10, 55, ExpectedResult = -5)]
+        [TestCase(20, 35, -10, 55, ExpectedResult = 5)]
         [TestCase(21, 84, 42, 1071, ExpectedResult = 21)]
         public int GetGCD_Test_With_ManyArguments(int first, int second,int third,int forth)
         {
-            return EucludianMathematicalAlgorithms.GetGCD(first, second,third,forth);
+            return EucludianMathematicalAlgorithms.GetGCD(methodGDC,first, second,third,forth);
         }
 
-        [TestCase(1)]
-        public void EucludianMathematicalAlgorithms_ArgumentExeption(int oneParam)
+        [Test]
+        public void EucludianMathematicalAlgorithms_ArgumentExeption()
         {
-            Assert.Throws<ArgumentException>(() => EucludianMathematicalAlgorithms.GetGCD());
+            Assert.Throws<ArgumentException>(() => EucludianMathematicalAlgorithms.GetGCD(methodGDC));
         }
+
+        [TestCase(10, 15, ExpectedResult = 5)]
+        [TestCase(20, 10, ExpectedResult = 10)]
+        [TestCase(20, 0, ExpectedResult = 20)]
+        [TestCase(20, -15, ExpectedResult = 5)]
+        public int BinaryEuclidianAlgorithmWithTwoNumbers(int first, int second)
+        {
+            return EucludianMathematicalAlgorithms.BinaryGCDalgorithm(first, second, methodBinary);
+        }
+#region Method for delegate
+        public static int GDCMethodForDelegate(int first, int second)
+        {
+            if (first == 0 && second == 0)
+                throw new ArgumentException();
+            if (first == 0 || first == second)
+                return Math.Abs(second);
+            if (second == 0)
+                return Math.Abs(first);
+
+            if (second > first)
+            {
+                int complete = first;
+                first = second;
+                second = complete;
+            }
+            int residual = 1;
+
+            while (residual != 0)
+            {
+                var temp = Math.Abs(first / second);
+                if (temp == 0)
+                    temp = 1;
+                residual = Math.Abs(first - (second * temp));
+                first = second;
+                second = residual;
+            }
+
+            return first;
+        }
+
+        public static int BinaryMethodForDelegate(int first, int second)
+        {
+            if (first == second)
+            {
+                return first;
+            }
+            if (first == 1 || second == 1)
+            {
+                return 1;
+            }
+
+            if (first == 0 && second == 0) throw new Exception("Invalid function arguments!");
+
+            if (first == 0)
+            {
+                return second;
+            }
+
+            if (second == 0)
+            {
+                return first;
+            }
+            if ((first % 2 == 0) && (second % 2 == 0))
+            {
+                return 2 * BinaryMethodForDelegate(first / 2, second / 2);
+            }
+            if ((first % 2 == 0) && (second % 2 != 0))
+            {
+                return BinaryMethodForDelegate(first / 2, second);
+            }
+            if ((first % 2 != 0) && (second % 2 == 0))
+            {
+                return BinaryMethodForDelegate(first, second / 2);
+            }
+            return BinaryMethodForDelegate(second, Math.Abs(first - second));
+        }
+#endregion
     }
 }
+
+
+
--- EPAM.Summer.Day2.Zheldak/Task2/EucludianMathematicalAlgorithms.cs
+++ EPAM.Summer.Day2.Zheldak/Task2/EucludianMathematicalAlgorithms.cs
@@ -4,47 +4,24 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Task2;
 namespace Task2
 {
+    public delegate int EuclidianDelegate(int a, int b);
     public static class EucludianMathematicalAlgorithms
     {
+       
+        
         /// <summary>
         /// The method for getting the greatest common divisor of two numbers.
         /// </summary>
         /// <param name="first"></param>
         /// <param name="second"></param>
+        /// <param name="methodDelegate"></param>
         /// <returns>The greatest common divisor of two numbers</returns>
-        public static int GetGCD(int first, int second)
-        {
-            if (first == 0 && second == 0)
-                throw new ArgumentException();
-            if (first == 0 || first == second)
-                return Math.Abs(second);
-            if (second == 0)
-                return Math.Abs(first);
-
-            if (second > first)
-            {
-                int complete = first;
-                first = second;
-                second = complete;
-            }
-            int residual = 1;
-
-            while (residual != 0)
-            {
-                var temp = Math.Abs(first / second);
-                if (temp == 0)
-                    temp = 1;
-                residual = Math.Abs(first - (second * temp));
-                first = second;
-                second = residual;
-            }
-
-            return first;
+        public static int GetGCD(int first, int second, EuclidianDelegate methodDelegate)
+            => methodDelegate(first, second);
         
-    }
         /// <summary>
         /// The method for getting the greatest common divisor of two numbers and receiving method execution time.
         /// </summary>
@@ -52,12 +29,12 @@ namespace Task2
         /// <param name="second">Second value</param>
         /// <param name="elapsedTime">Out param for elapsed time in millisecs</param>
         /// <returns>The greatest common divisor of two numbers</returns>
-        public static int GetGCD(int first, int second, out long elapsedTime)
+        public static int GetGCD(int first, int second, out long elapsedTime,EuclidianDelegate methodDelegate)
         {
             var stopWatch = new Stopwatch();
             stopWatch.Start();
 
-            int result = GetGCD(first, second);
+            int result = methodDelegate(first, second);
 
             stopWatch.Stop();
             elapsedTime = stopWatch.ElapsedTicks;
@@ -71,10 +48,10 @@ namespace Task2
         /// <param name="second"></param>
         /// <param name="third"></param>
         /// <returns>The greatest common divisor</returns>
-        public static int GetGCD(int first, int second, int third)
+        public static int GetGCD(int first, int second, int third, EuclidianDelegate methodDelegate)
         {
-            int intermediate = GetGCD(first, second);
-            int result = GetGCD(intermediate, third);
+            int intermediate = methodDelegate(first, second);
+            int result = methodDelegate(intermediate, third);
 
             return result;
         }
@@ -86,24 +63,26 @@ namespace Task2
         /// <param name="third"></param>
         /// <param name="elapsedTime"></param>
         /// <returns>Great common devisor</returns>
-        public static int GetGCD(int first, int second, int third, out long elapsedTime)
+        public static int GetGCD(int first, int second, int third, out long elapsedTime, EuclidianDelegate methodDelegate)
         {
             var stopWatch = new Stopwatch();
             stopWatch.Start();
 
-            int result = GetGCD(first, second, third);
+            int result = GetGCD(first, second, third, methodDelegate);
 
             stopWatch.Stop();
             elapsedTime = stopWatch.ElapsedTicks;
 
             return result;
         }
+
         /// <summary>
         /// The method for getting the greatest common divisor(GCD).
         /// </summary>
+        /// <param name="methodDelegate"></param>
         /// <param name="arrayNumbers"></param>
         /// <returns></returns>
-        public static int GetGCD(params int[] arrayNumbers)
+        public static int GetGCD(EuclidianDelegate methodDelegate,params int[] arrayNumbers)
         {
             Array.Sort(arrayNumbers);
 
@@ -114,19 +93,21 @@ namespace Task2
             int[] resultArray = new int[arrayNumbers.Length - 1];
             for (int i = 0; i < arrayNumbers.Length - 1; i++)
             {
-                resultArray[i] = GetGCD(arrayNumbers[0], arrayNumbers[i + 1]);
+                resultArray[i] = GetGCD(arrayNumbers[0], arrayNumbers[i + 1], methodDelegate);
             }
 
             return resultArray.Min();
         }
+
         /// <summary>
         /// The method for getting the greatest common divisor(GCD) and receiving method execution time.
         /// <exception cref="ArgumentException">Throws when you have less two params</exception>
         /// </summary>
         /// <param name="elapsedTime"> elapsed time</param>
+        /// <param name="methodDelegate"></param>
         /// <param name="arrayNumbers">count of params</param>
         /// <returns>Grear common devisir</returns>
-        public static int GetGCD(out long elapsedTime, params int[] arrayNumbers)
+        public static int GetGCD(out long elapsedTime, EuclidianDelegate methodDelegate, params int[] arrayNumbers)
         {
             var stopWatch = new Stopwatch();
 
@@ -140,7 +121,7 @@ namespace Task2
             int[] resultArray = new int[arrayNumbers.Length - 1];
             for (int i = 0; i < arrayNumbers.Length - 1; i++)
             {
-                resultArray[i] = GetGCD(arrayNumbers[0], arrayNumbers[i + 1]);
+                resultArray[i] = GetGCD(arrayNumbers[0], arrayNumbers[i + 1], methodDelegate);
             }
 
             stopWatch.Stop();
@@ -148,73 +129,54 @@ namespace Task2
 
             return resultArray.Min();
         }
+
         /// <summary>
         /// The method for getting the greatest common divisor of two numbers.
         /// </summary>
         /// <param name="first">first value</param>
         /// <param name="second">second value</param>
+        /// <param name="methodDelegate"></param>
         /// <returns>The greatest common divisor</returns>
-        public static int BinaryGCDalgorithm(int first, int second)
-        {
-            if (first == 0 && second == 0) throw new ArgumentException();
-            if (first == second)
-                return first;
-            if (first == 0)
-                return second;
-            if (second == 0)
-                return first;
+        public static int BinaryGCDalgorithm(int first, int second, EuclidianDelegate methodDelegate)
+            => methodDelegate(first, second);
 
-            if (first % 2 == 0)
-            {
-                if (second % 2 == 0)
-                {
-                    return 2 * BinaryGCDalgorithm(first / 2, second / 2);
-                }
-                return BinaryGCDalgorithm(first / 2, second);
-            }
-
-            if (second % 2 == 0)
-            {
-                return BinaryGCDalgorithm(first, second / 2);
-            }
-            if (first > second)
-                return BinaryGCDalgorithm((first - second) / 2, second);
-
-            return BinaryGCDalgorithm((second - first) / 2, first);
-        }
         /// <summary>
         /// The method for getting the greatest common divisor of two numbers and receiving method execution time.
         /// </summary>
         /// <param name="first"> First value</param>
         /// <param name="second">Second value</param>
         /// <param name="elapsedTime">Out param for elapsed time in millisecs</param>
+        /// <param name="methodDelegate"></param>
         /// <returns>The greatest common divisor of two numbers</returns>
-        public static int BinaryGCDalgorithm(int first, int second, out long elapsedTime)
+        public static int BinaryGCDalgorithm(int first, int second, out long elapsedTime, EuclidianDelegate methodDelegate)
         {
             var stopWatch = new Stopwatch();
             stopWatch.Start();
 
-            int result = BinaryGCDalgorithm(first, second);
+            int result = methodDelegate(first,second);
 
             stopWatch.Stop();
             elapsedTime = stopWatch.ElapsedTicks;
 
             return result;
         }
+
         /// <summary>
         /// The method for getting the greatest common divisor of three numbers.
         /// </summary>
         /// <param name="first">first value</param>
         /// <param name="second">second value</param>
         /// <param name="third">third value</param>
+        /// <param name="methodDelegate"></param>
         /// <returns>The greatest common divisor</returns>
-        public static int BinaryGCDalgorithm(int first, int second,int third)
+        public static int BinaryGCDalgorithm(int first, int second,int third, EuclidianDelegate methodDelegate)
         {
-            int intermediate = BinaryGCDalgorithm(first, second);
-            int result = BinaryGCDalgorithm(intermediate,third);
+            int intermediate = methodDelegate(first, second);
+            int result = methodDelegate(intermediate,third);
 
             return result;
         }
+
         /// <summary>
         /// The method for getting the greatest common divisor of three numbers and receiving method execution time.
         /// </summary>
@@ -222,25 +184,28 @@ namespace Task2
         /// <param name="second"></param>
         /// <param name="third"></param>
         /// <param name="elapsedTime"></param>
+        /// <param name="methodDelegate"></param>
         /// <returns>great common devisor</returns>
-        public static int BinaryGCDalgorithm(int first, int second, int third,out long elapsedTime)
+        public static int BinaryGCDalgorithm(int first, int second, int third,out long elapsedTime, EuclidianDelegate methodDelegate)
         {
             var stopWatch = new Stopwatch();
             stopWatch.Start();
 
-            int result = BinaryGCDalgorithm(first, second, third);
+            int result = BinaryGCDalgorithm(first, second, third,methodDelegate);
 
             stopWatch.Stop();
             elapsedTime = stopWatch.ElapsedTicks;
 
             return result;
         }
+
         /// <summary>
         /// The method for getting the greatest common divisor(GCD).
         /// </summary>
+        /// <param name="methodDelegate"></param>
         /// <param name="arrayNumbers">Count numbers</param>
         /// <returns>The greatest common divisor</returns>
-        public static int BinaryGCDalgorithm(params int[] arrayNumbers)
+        public static int BinaryGCDalgorithm(EuclidianDelegate methodDelegate,params int[] arrayNumbers)
         {
             if (arrayNumbers.Length < 2)
             {
@@ -252,19 +217,21 @@ namespace Task2
 
             for (int i = 0; i < arrayNumbers.Length - 1; i++)
             {
-                resultArray[i] = BinaryGCDalgorithm(arrayNumbers[0], arrayNumbers[i + 1]);
+                resultArray[i] = BinaryGCDalgorithm(arrayNumbers[0], arrayNumbers[i + 1],methodDelegate);
             }
 
             return resultArray.Min();
         }
+
         /// <summary>
         /// The method for getting the greatest common divisor(GCD) and receiving method execution time.
         /// <exception cref="ArgumentException">Throws when you have less two params</exception>
         /// </summary>
         /// <param name="elapsedTime"> elapsed time</param>
+        /// <param name="methodDelegate"></param>
         /// <param name="arrayNumbers">count of params</param>
         /// <returns>Grear common devisir</returns>
-        public static int BinaryGCDalgorithm(out long elapsedTime, params int[] arrayNumbers)
+        public static int BinaryGCDalgorithm(out long elapsedTime, EuclidianDelegate methodDelegate, params int[] arrayNumbers)
         {
             var stopWatch = new Stopwatch();
 
@@ -278,7 +245,7 @@ namespace Task2
             int[] resultArray = new int[arrayNumbers.Length - 1];
             for (int i = 0; i < arrayNumbers.Length - 1; i++)
             {
-                resultArray[i] = BinaryGCDalgorithm(arrayNumbers[0], arrayNumbers[i + 1]);
+                resultArray[i] = BinaryGCDalgorithm(arrayNumbers[0], arrayNumbers[i + 1],methodDelegate);
             }
 
             stopWatch.Stop();
